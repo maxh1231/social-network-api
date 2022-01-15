2a7aa709d8591b9e056db9a6be7f20268dbb2d44
maxh1231 <maxhumpherys@gmail.com>
2022-01-15 12:44:26 -0700

    get all users and create user

---
+++ controllers/user-controller.js
@@ -0,0 +1,29 @@
+const { User } = require('../models');
+
+const userController = {
+
+    // get all users
+    getAllUser(req, res) {
+        User.find({})
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    // create user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = userController;
